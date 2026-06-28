c130cfbac15f37d02929a8c37bd9d41f5b8b3d38
george dinu <george.dinu.ro@gmail.com>
2026-06-28 19:05:27 +0300

    add POST create endpoint

---
+++ src/main/java/my/work/springbootcrudvalidation/controller/DepartmentController.java
@@ -0,0 +1,26 @@
+package my.work.springbootcrudvalidation.controller;
+
+import lombok.RequiredArgsConstructor;
+import my.work.springbootcrudvalidation.projection.DepartmentProjection;
+import my.work.springbootcrudvalidation.service.DepartmentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/departments")
+@RestController
+@RequiredArgsConstructor
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    DepartmentProjection create(@RequestBody DepartmentProjection projection) {
+        return departmentService.create(projection);
+    }
+
+}
---
+++ src/main/java/my/work/springbootcrudvalidation/service/DepartmentService.java
@@ -0,0 +1,24 @@
+package my.work.springbootcrudvalidation.service;
+
+import lombok.RequiredArgsConstructor;
+import my.work.springbootcrudvalidation.mapper.DepartmentMapper;
+import my.work.springbootcrudvalidation.projection.DepartmentProjection;
+import my.work.springbootcrudvalidation.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    private final DepartmentMapper departmentMapper;
+
+    public DepartmentProjection create(DepartmentProjection projection) {
+        var entity = departmentMapper.toEntity(projection);
+        entity = departmentRepository.save(entity);
+
+        return departmentMapper.toProjection(entity);
+    }
+
+}
