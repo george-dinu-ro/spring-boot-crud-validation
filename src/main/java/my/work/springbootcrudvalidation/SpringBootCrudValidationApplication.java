4528f72536f9b733e692c8cf8b4e1e797c2a9f42
george dinu <george.dinu.ro@gmail.com>
2026-06-28 14:44:26 +0300

    first commit

---
+++ src/main/java/my/work/springbootcrudvalidation/SpringBootCrudValidationApplication.java
@@ -0,0 +1,13 @@
+package my.work.springbootcrudvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/work/springbootcrudvalidation/SpringBootCrudValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package my.work.springbootcrudvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
